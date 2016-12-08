13a6c7a46fa3157d6fbaed2054f5cf49fea25638
vikhyatgoyal <vigo9910@colorado.edu>
2016-12-08 00:28:46 -0700

    added documentation

--- Includes/BASE/ds1631.h
+++ Includes/BASE/ds1631.h
@@ -12,7 +12,20 @@
 #define CMD_START_CONVERSION 0x51
 #define CMD_READ_TEMP 0xAA
 
+/********************************************************************/
+/*description: this function configures external
+   temperature sensor and starts continuous conversion
+/*parameter: void										            */
+/*return:    void						                            */
+/********************************************************************/
 void DS1631_Start_Continuos_Conversion (void);
+
+/********************************************************************/
+/*description: this function reads the last converted value
+				over I2C from the external sensor
+/*parameter: void										            */
+/*return:    float value of last read temperature                   */
+/********************************************************************/
 float DS1631_Read_Temperature (void);
 
 
