07ece95b9f4c18ac4ef1419bda4361a56e7cf626
vikhyatgoyal <vigo9910@colorado.edu>
2016-12-08 00:27:44 -0700

    added documentation

--- Includes/BASE/mma8451.h
+++ Includes/BASE/mma8451.h
@@ -8,12 +8,39 @@
 
 extern int16_t resultx, resulty, resultz;
 
+/********************************************************************/
+/*description: this function initializes the I2C used for acc sensor*/
+/*parameter: void				 								    */
+/*return:    void								                    */
+/********************************************************************/
 void hal_dev_mma8451_init(void); //prepare I2C port
+
+/********************************************************************/
+/*description: this function reads the data to acc sensor reg      */
+/*parameter: void				 								    */
+/*return:    void								                    */
+/********************************************************************/
 uint8_t   hal_dev_mma8451_read_reg(uint8_t addr);
+
+/********************************************************************/
+/*description: this function writes the data to acc sensor reg      */
+/*parameter: void				 								    */
+/*return:    void								                    */
+/********************************************************************/
 void hal_dev_mma8451_write_reg(uint8_t addr, uint8_t data);
-uint16_t hal_dev_mma8451_test_reg_rw(void);
-void hal_dev_mma8451_test_function(void);
+
+/********************************************************************/
+/*description: this function initializes the acc sensor				*/
+/*parameter: void				 								    */
+/*return:    void								                    */
+/********************************************************************/
 void accel_init(void);
+
+/********************************************************************/
+/*description: this function reads the x,y,z value from acc sensor*/
+/*parameter: void				 								    */
+/*return:    void								                    */
+/********************************************************************/
 void accel_read(void);
 
 
