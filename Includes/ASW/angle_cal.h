43f40612906057c26404356d161b60fde1d083a8
vikhyatgoyal <vigo9910@colorado.edu>
2016-12-08 00:29:46 -0700

    added documentation

--- Includes/ASW/angle_cal.h
+++ Includes/ASW/angle_cal.h
@@ -2,7 +2,12 @@
 #ifndef Angle_H_
 #define Angle_H_
 
-
+/********************************************************************/
+/*description: This functions updates the orientation 				*/
+/* of the car depending on ax sensor value                          */
+/*parameter: void      	       							            */
+/*return:    void						                            */
+/********************************************************************/
 extern void angle_calculation(void);
 
 #endif
