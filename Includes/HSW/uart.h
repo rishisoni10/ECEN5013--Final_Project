300a2c632648c607edd1981a1c9a056395fa139c
vikhyatgoyal <vigo9910@colorado.edu>
2016-12-08 00:32:12 -0700

    added documentation

--- Includes/HSW/uart.h
+++ Includes/HSW/uart.h
@@ -10,7 +10,18 @@
 
 #include <stdint.h>
 
+/********************************************************************/
+/*description: this function initializes the UART 					*/
+/*parameter: void				 								    */
+/*return:    void								                    */
+/********************************************************************/
 void uart_init(uint32_t baud);
+
+/********************************************************************/
+/*description: this function is the interrupt handler for uart		*/
+/*parameter: void				 								    */
+/*return:    void								                    */
+/********************************************************************/
 void UART0_IRQHandler (void);
 
 #define BAUD_RATE       38400
