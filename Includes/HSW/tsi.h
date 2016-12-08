6a25d023c95be18fbaa73795af3546c37e4a7f0d
vikhyatgoyal <vigo9910@colorado.edu>
2016-12-08 00:34:30 -0700

    added documentation

--- Includes/HSW/tsi.h
+++ Includes/HSW/tsi.h
@@ -8,8 +8,25 @@
 #ifndef INCLUDES_FIRMWARE_I_TSI_H_
 #define INCLUDES_FIRMWARE_I_TSI_H_
 
+/********************************************************************/
+/*description: this function initializes the specified TSI channel.	*/
+/*parameter: channel   		 								        */
+/*return:    void								                    */
+/********************************************************************/
 void touch_init(uint32_t channel_mask);
+
+/********************************************************************/
+/*description: this function reads the value from specified TSI channel.	*/
+/*parameter: channel   		 								        */
+/*return:    void								                    */
+/********************************************************************/
 int touch_data(int channel);
+
+/********************************************************************/
+/*description: this function starts the conversion for specified TSI channel.	*/
+/*parameter: channel   		 								        */
+/*return:    void								                    */
+/********************************************************************/
 inline static void scan_start(int channel);
 inline static uint16_t scan_data(void);
 void TSI0_IRQHandler(void);
