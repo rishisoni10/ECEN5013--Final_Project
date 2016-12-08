febe97990f85fe55b048c3917194c09ce77eb12e
vikhyatgoyal <vigo9910@colorado.edu>
2016-12-08 00:27:14 -0700

    added documentation

--- Includes/BASE/profiler.h
+++ Includes/BASE/profiler.h
@@ -12,7 +12,19 @@
 
 #define DATA_LEN_MAX    5000
 
+/********************************************************************/
+/*description: this function initializes and configures
+ * the timer used for task scheme      								*/
+/*parameter: void				 								    */
+/*return:    void								                    */
+/********************************************************************/
 void TaskTimer_init(void);
+
+/********************************************************************/
+/*description: this function stops the task scheme timer			*/
+/*parameter: void				 								    */
+/*return:    void								                    */
+/********************************************************************/
 void stop_TaskTimer(void);
 
 #endif /* SOURCES_PROFILE_H_ */
