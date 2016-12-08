9aa09e3f1f1cb67eb45cae30d5c98a73c7bde36f
vikhyatgoyal <vigo9910@colorado.edu>
2016-12-07 23:31:34 -0700

    updated orientation cal

--- Includes/ASW/angle_cal.h
+++ Includes/ASW/angle_cal.h
@@ -2,31 +2,7 @@
 #ifndef Angle_H_
 #define Angle_H_
 
-#define NUM_MEDIUM_SAMPLES 8
-
-struct different_samples
-  {
- unsigned char   samples;
- int buffer_samples[NUM_MEDIUM_SAMPLES];
-  };
-
-
-/*Acceleration  RAM */
-extern signed int X_acc;
-extern signed int Y_acc;
-extern signed int Z_acc;
-
-extern unsigned int xy_mag;
-extern unsigned int xz_mag;
-extern unsigned int yz_mag;
-
-extern signed  int xy_angle;
-extern signed  int xz_angle;
-extern signed  int yz_angle;
-
-extern unsigned char fall_input;
 
 extern void angle_calculation(void);
-void detect_fall_detection(void);
 
 #endif
