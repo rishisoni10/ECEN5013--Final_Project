abdf7ed722849610cfbd85b6eed9fc3cd652e6aa
vikhyatgoyal <vigo9910@colorado.edu>
2016-12-07 23:36:01 -0700

    updated temp read

--- Includes/BASE/ds1631.h
+++ Includes/BASE/ds1631.h
@@ -1,18 +1,19 @@
 /*
  * ds1631.h
  *
- *  Created on: Nov 21, 2016
- *      Author: vikhyat
+ *  Created on: 20-Nov-2016
+ *      Author: Vikhyat
  */
+
 #ifndef DS1631_H_
 #define DS1631_H_
 
-
-#define DS1631_SLAVE_ADDRESS 0x48
+#define DS1631_SLAVE_ID 0x48
 #define CMD_START_CONVERSION 0x51
 #define CMD_READ_TEMP 0xAA
 
-void start_Temperature_Conversion (void);
-float read_Temperature (void);
+void DS1631_Start_Continuos_Conversion (void);
+float DS1631_Read_Temperature (void);
+
 
 #endif
